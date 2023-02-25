d6ba3c383166b7b9aa1982c0408afd08b6a94f70
Aizhan Sagitzhanova(190103134) <78720887+SagitzhanovaAizhan@users.noreply.github.com>
2023-02-25 20:06:40 +0600

    Create the Animal class

---
+++ problem-B/Animal.java
@@ -0,0 +1,12 @@
+public abstract class Animalllll {
+   protect int legs;
+   protected Animal (int legs) {
+   this.legs = legs;
+   
+   public abstract void eat ();
+    public void walk () {
+    System.ou.println("This animal walks on " + legs + "legs.");
+}
+    
+}
+}
