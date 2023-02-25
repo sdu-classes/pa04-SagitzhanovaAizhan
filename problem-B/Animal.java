cc63cc5a8d6e2c6898403361c305e42be10442d0
Aizhan Sagitzhanova(190103134) <78720887+SagitzhanovaAizhan@users.noreply.github.com>
2023-02-25 22:24:47 +0600

    Update Animal.java

--- problem-B/Animal.java
+++ problem-B/Animal.java
@@ -1,1 +1,14 @@
-
+public abstract class Animal{
+  pritected int legs;
+  
+  protected Animal(int legs)
+  {
+    this.legs=legs;
+  }
+  
+  public vois walk()
+  {
+    System.out.println("this animal walks by "+lega+"legs.");
+  }
+  public abstract vois eat();
+}
